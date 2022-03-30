ec9600977eb37ba340ef7f095caf2945e0ed0281
Gargi Chakraverty <gargichakraverty2@gmail.com>
2022-03-30 20:34:22 +0530

    final commit

--- server.js
+++ server.js
@@ -1,36 +1,62 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const ShortUrl = require('./models/shortUrl')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
-const dbURI="mongodb+srv://gargi1234:urlshortner@cluster0.xtrmi.mongodb.net/urlShortnerDatabase?retryWrites=true&w=majority"
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+const ShortUrl = require('./models/shortUrl');
+const app = express();
+
+
+app.use(express.static('public'));
+
+const dbURI = process.env.dbURI;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(process.env.PORT || 5000))
   .catch(err => console.log(err));
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find()
-  res.render('index', { shortUrls: shortUrls })
-})
+  try {
+    const shortUrls = await ShortUrl.find();
+    res.render('index', { shortUrls: shortUrls });
+  }
+  catch (err) {
+    res.json({ message: err.message });
+  }
+  
+});
 
-app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
-
-  res.redirect('/')
+app.post('/', async (req, res) => {
+  try {
+    await ShortUrl.create({ full: req.body.fullUrl });
+    res.redirect('/');
+  }
+  catch (err)
+  {
+    res.json({ message: err.message });
+  }
+  
 })
 
 app.get('/:shortUrl', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
+  try {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+    if (shortUrl == null)
+      return res.sendStatus(404);
 
-  shortUrl.clicks++
-  shortUrl.save()
-
-  res.redirect(shortUrl.full)
+    shortUrl.clicks++;
+    shortUrl.save();
+    res.redirect(shortUrl.full);
+  }
+  catch (err)
+  {
+    res.json({ message: err.message });
+  }
+ 
 })
 
 
